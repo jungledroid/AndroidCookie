68c681828d91b496400e1993bba4c4a630563a77
Joez <Joez@xogrp.com>
2016-05-25 04:33:57 +0800

    add some options for filterafter mode

--- app/src/main/java/com/example/joez/androidcookie/BitmaskActivity.java
+++ app/src/main/java/com/example/joez/androidcookie/BitmaskActivity.java
@@ -7,15 +7,20 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 public class BitmaskActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "BitmaskActivity";
+    private static final String MODE_SINGLE="SingleMode";
+    private static final String MODE_MULTI = "MultiMode";
+    private static final String MODE_FILLAFTER="FillAfterMode";
     private AnimateModeController mAnimateController;
+    private CheckBox mCbRotation,mCbTransitionY,mCbTransitionX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +34,16 @@ public class BitmaskActivity extends AppCompatActivity implements View.OnClickLi
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setTitle(TAG);
 
-        ImageView ivTarget = (ImageView)findViewById(R.id.iv_image);
         findViewById(R.id.btn_rotation).setOnClickListener(this);
         findViewById(R.id.btn_trantionY).setOnClickListener(this);
         findViewById(R.id.btn_transitonX).setOnClickListener(this);
         findViewById(R.id.btn_mode).setOnClickListener(this);
+        findViewById(R.id.btn_animate).setOnClickListener(this);
+        mCbRotation = (CheckBox)findViewById(R.id.cb_rotation);
+        mCbTransitionY = (CheckBox)findViewById(R.id.cb_tanstionY);
+        mCbTransitionX = (CheckBox)findViewById(R.id.cb_transitionX);
 
-        mAnimateController = new AnimateModeController(ivTarget);
+        mAnimateController = new AnimateModeController(findViewById(R.id.iv_image));
     }
 
     @Override
@@ -58,63 +66,115 @@ public class BitmaskActivity extends AppCompatActivity implements View.OnClickLi
                 mAnimateController.animateImageByFlag(AnimateModeController.TRANSITIONX_MASK);
                 break;
             case R.id.btn_mode:
-                mAnimateController.animateImageByFlag(AnimateModeController.STATE_MODE_SINGLE_MASK);
-                ((Button)v).setText(mAnimateController.hasStatus(AnimateModeController.STATE_MODE_SINGLE_MASK)?"SingleMode":"MultiMode");
+                boolean isSingleMode = mAnimateController.hasStatus(AnimateModeController.STATE_MODE_SINGLE_MASK);
+                boolean isFillAfterMode = mAnimateController.hasStatus(AnimateModeController.STATE_MODE_FILAFTER_MASK);
+                boolean isCheckable = false;
+                if(isFillAfterMode){
+                    ((Button)v).setText(MODE_FILLAFTER);
+                    mAnimateController.animateWithMode(AnimateModeController.STATE_MODE_SINGLE_MASK);
+                    ((Button) v).setText( MODE_SINGLE);
+                }else {
+                    isCheckable = isSingleMode;
+                    mAnimateController.animateWithMode(isSingleMode?AnimateModeController.STATE_MODE_MULTI_MASK:AnimateModeController.STATE_MODE_FILAFTER_MASK);
+                    ((Button) v).setText(isSingleMode ?MODE_MULTI:MODE_FILLAFTER);
+                }
+                if(!isCheckable) {
+                    mCbRotation.setChecked(isCheckable);
+                    mCbTransitionX.setChecked(isCheckable);
+                    mCbTransitionY.setChecked(isCheckable);
+                }
+                mCbRotation.setClickable(isCheckable);
+                mCbTransitionX.setClickable(isCheckable);
+                mCbTransitionY.setClickable(isCheckable);
                 break;
+            case R.id.btn_animate:
+                int flag = 0;
+                if(mCbRotation.isChecked()){
+                    flag|=AnimateModeController.ROTATION_MASK;
+                }
+                if(mCbTransitionY.isChecked()){
+                    flag|=AnimateModeController.TRANSITIONY_MASK;
+                }
+                if(mCbTransitionX.isChecked()){
+                    flag|=AnimateModeController.TRANSITIONX_MASK;
+                }
+                mAnimateController.animateImageByFlag(flag);
+
         }
     }
 
-    private static class AnimateModeController {
-        private int mViewFlag;
+    private static final class AnimateModeController {
+        private int mViewFlag = STATE_MODE_MULTI_MASK;
         public static final int ROTATION_MASK = 0x1;
         public static final int TRANSITIONY_MASK =0x2;
         public static final int TRANSITIONX_MASK =0x4;
         private static final int ANIMATE_STATE_MASK=0x7;
         public static final int STATE_MODE_SINGLE_MASK =0x8;
+        public static final int STATE_MODE_MULTI_MASK =0x0;
+        public static final int STATE_MODE_FILAFTER_MASK=0x10;
+        public static final int STATE_MODE_VISIBLE=0x1F;
         private ViewPropertyAnimatorCompat mViewCompat;
-
         public AnimateModeController(View view){
             mViewCompat = ViewCompat.animate(view);
         }
 
-        @IntDef({ROTATION_MASK, TRANSITIONY_MASK, TRANSITIONX_MASK,STATE_MODE_SINGLE_MASK})
+        @IntDef(flag=true,value={ROTATION_MASK, TRANSITIONY_MASK, TRANSITIONX_MASK,STATE_MODE_SINGLE_MASK,STATE_MODE_FILAFTER_MASK,STATE_MODE_MULTI_MASK})
         @Retention(RetentionPolicy.SOURCE)
         public @interface AnimateState{}
 
+        private void animateWithMode(@AnimateState int flag){
+            clearAllSate();
+            mViewFlag |= flag;
+            mViewCompat.rotation(0).translationX(0).translationY(0).setDuration(500).start();
+        }
+
         private void animateImageByFlag(@AnimateState int flag){
-            boolean hasStatus = hasStatus(flag);
-            if(hasStatus(STATE_MODE_SINGLE_MASK)){
-                clearAniamteState();
+            Log.e(TAG, "animateImageByFlag: flag:"+flag );
+            boolean isFilterAfterMode = hasStatus(STATE_MODE_FILAFTER_MASK);
+            if(isFilterAfterMode){
+                boolean hasStatus = hasStatus(flag);
+//                if (hasStatus) {
+//                    mViewFlag &= ~flag;
+//                } else {
+                    mViewFlag |= flag;
+//                }
+//                mViewCompat.rotation(0).translationX(0).translationY(0);
+            }else {
+                clearAnimateState();
                 mViewCompat.rotation(0).translationX(0).translationY(0);
             }
-            if(hasStatus){
-                mViewFlag&=~flag;
-            }else{
-                mViewFlag|=flag;
+            Log.e(TAG, "animateImageByFlag: --requestandroat:-"+requestForState(flag,ROTATION_MASK)+"---flag:"+flag );
+            if((requestForState(flag,ROTATION_MASK))&&hasStatus((~ROTATION_MASK|STATE_MODE_FILAFTER_MASK))||(!isFilterAfterMode&&requestForState(flag,ROTATION_MASK))){
+                mViewCompat.rotation(30);
             }
-            switch (flag){
-                case ROTATION_MASK:
-                    mViewCompat.rotation(hasStatus?0:30);
-                    break;
-                case TRANSITIONY_MASK:
-                    mViewCompat.translationY(hasStatus?0:200);
-                    break;
-                case TRANSITIONX_MASK:
-                    mViewCompat.translationX(hasStatus?0:200);
-                    break;
-                case STATE_MODE_SINGLE_MASK:
-                    clearAniamteState();
-                    break;
+            if((requestForState(flag,TRANSITIONY_MASK))&&hasStatus(~TRANSITIONY_MASK|STATE_MODE_FILAFTER_MASK)||(!isFilterAfterMode&&requestForState(flag,TRANSITIONY_MASK))){
+                mViewCompat.translationY(200);
+            }
+
+            if(requestForState(flag,TRANSITIONX_MASK)&&hasStatus(~TRANSITIONX_MASK|STATE_MODE_FILAFTER_MASK)||(!isFilterAfterMode&&requestForState(flag,TRANSITIONX_MASK))){
+                mViewCompat.translationX(200);
             }
             mViewCompat.setDuration(500).start();
         }
 
-        private boolean hasStatus(int flag){
+        private boolean requestForState(@AnimateState int request,@AnimateState int targetState){
+            return (request&targetState) != 0;
+        }
+
+        private boolean hasStatus(@AnimateState int flag){
             return (mViewFlag&flag)>0;
         }
 
-        private void clearAniamteState(){
+        private void clearAnimateState(){
             mViewFlag&=~ANIMATE_STATE_MASK;
+        }
+
+        private void clearAllSate(){
+            mViewFlag&=~STATE_MODE_VISIBLE;
+        }
+
+        private void setFlag(@AnimateState int flag){
+            mViewFlag|=flag;
         }
     }
 
